59525e36c7f563bcd6349503e3f06ddcd2d5bcef
SHIBAMMOHANTY <106046613+SHIBAMMOHANTY@users.noreply.github.com>
2026-01-30 11:15:12 +0530

    565

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"4l7bS9UMrmSeMgbWXZodcjgaqh7GylVyqxCI05OKOoo=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
