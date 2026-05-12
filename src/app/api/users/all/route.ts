0320ec6bbe1a43cc9df829b7d645d167e6b441bc
SHIBAMMOHANTY <shibammohanty8658@gmail.com>
2026-05-12 17:33:33 +0530

    refactor: remove redundant OPTIONS function and streamline CORS header handling

--- src/app/api/users/all/route.ts
+++ src/app/api/users/all/route.ts
@@ -46,10 +46,3 @@ export async function GET(req: NextRequest) {
     );
   }
 }
-export async function OPTIONS() {
-  const response = new NextResponse(null, { status: 204 });
-  response.headers.set('Access-Control-Allow-Origin', '*');
-  response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  return response;
-}
