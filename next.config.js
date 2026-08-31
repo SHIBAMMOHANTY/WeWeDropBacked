2507ec08530f42bb12c77e299fe3b5c955d1586a
Shibam <106046613+SHIBAMMOHANTY@users.noreply.github.com>
2026-09-01 01:49:22 +0530

    Fix invoice route

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  experimental: {
+    serverComponentsExternalPackages: ['pdfkit'],
+  },
   async headers() {
     return [
       {
--- src/lib/invoice.ts
+++ src/lib/invoice.ts
@@ -1,6 +1,6 @@
 import fs from 'fs';
 import path from 'path';
-import PDFDocument from 'pdfkit/js/pdfkit.standalone';
+import PDFDocument from 'pdfkit';
 import { uploadToCloudinary } from './upload';
 import { prisma } from './prisma';
 
